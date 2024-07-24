8928d935628a76c4fcfeeb2192d8d87b8d038d35
Gundlapally Sreeteja <Sreetejagundlapally@gmail.com>
2024-07-24 13:27:55 +0530

    day02

---
+++ day2_loops_1.py
@@ -0,0 +1,18 @@
+my_list=list(map(int,input().split()))
+for i in range(len(my_list)):
+    print(my_list[i],end=" ")
+print("\n----------")
+for i in my_list:
+     print(i,end=" ")
+
+print("\n")
+
+
+s="hello world"
+for i in range(len(s)):
+    print(s[i],end=" ")
+print("\n")
+
+for i in s:
+    print(i,end=" ")
+
