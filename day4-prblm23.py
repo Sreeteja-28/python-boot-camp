d94f6c2bc55b7f2c4589229bdae6e8e045db5a06
Gundlapally Sreeteja <Sreetejagundlapally@gmail.com>
2024-07-24 14:24:06 +0530

    day04

---
+++ day4-prblm23.py
@@ -0,0 +1,10 @@
+'''print all the prime numbers in given range'''
+
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+for i in range(a,b+1):
+    for j in range(2,i):
+        if i%j==0:
+            break
+    else:
+        print(i)
