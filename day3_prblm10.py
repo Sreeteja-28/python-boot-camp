08ad9c18beb65977c2d88387fc82d4be1f10f06a
Gundlapally Sreeteja <Sreetejagundlapally@gmail.com>
2024-07-24 13:42:05 +0530

    day03

---
+++ day3_prblm10.py
@@ -0,0 +1,14 @@
+'''print the element in a paticular index '''
+my_list=list(map(int,input().split(" ")))
+k=int(input())
+idx=k%len(my_list)
+print(my_list[idx]) 
+#l=len(my_list)
+#for i in range(0,l):
+ #   if(i==k):
+  #   print(my_list[k])
+   #  break
+    #if(k>l):
+     #  n=k%l
+      # print(my_list[n])
+     
